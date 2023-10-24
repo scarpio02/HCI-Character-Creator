e22fa7ff03e8463a8604184c20c87221fdfe60b2
scarpio02 <scarpio@ufl.edu>
2023-10-24 15:06:34 -0400

    CG System

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -15,8 +15,8 @@ public class CameraMove : MonoBehaviour
     {
         Vector3 mousePos = Input.mousePosition;
 
-        Debug.Log(mousePos.x);
-        Debug.Log(mousePos.y);
+        //Debug.Log(mousePos.x);
+        //Debug.Log(mousePos.y);
 
         transform.position = new Vector3(0,3,0) - new Vector3((mousePos.x - (Screen.width / 2)) / 2000, 0, (mousePos.y - (Screen.height / 2)) / 2000);
     }
